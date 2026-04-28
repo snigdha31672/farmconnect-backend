6977a14714bfb31ee2f54de77e04283d949c6f87
Bareddy Reshnavi Devi <bareddyreshnavidevi@gmail.com>
2026-04-28 09:53:23 +0530

    Refactor ProductRepository to simplify search methods

--- farmconnect-backend/src/main/java/com/farmconnect/farmconnect_backend/repository/ProductRepository.java
+++ farmconnect-backend/src/main/java/com/farmconnect/farmconnect_backend/repository/ProductRepository.java
@@ -5,12 +5,9 @@ import com.farmconnect.farmconnect_backend.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByFarmer(User farmer, Pageable pageable);
     Page<Product> findByCategory(String category, Pageable pageable);
-    
-
-    @Query("SELECT p FROM Product p WHERE LOWER(p.name) LIKE LOWER(CONCAT('%',:query,'%')) OR LOWER(p.description) LIKE LOWER(CONCAT('%',:query,'%'))")
-    Page<Product> search(String query, Pageable pageable);
+    Page<Product> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
